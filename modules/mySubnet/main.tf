56e6770d1c2654d9e3e11747533855ffa253d875
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-17 05:47:19 -0400

    subnet testing

--- modules/mySubnet/main.tf
+++ modules/mySubnet/main.tf
@@ -51,8 +51,8 @@ resource "aws_route_table" "tf_private_rt" {
   vpc_id = "${var.vpc_id}"
 
   route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = "${var.vpc_igw_id}"
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${var.vpc_igw_id}"
   }
 
   tags {
