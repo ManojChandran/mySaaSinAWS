825d3ccdfa9a79e5cbe1e324f7c5f8713b41991f
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-17 05:42:59 -0400

    subnet testing

--- modules/mySubnet/main.tf
+++ modules/mySubnet/main.tf
@@ -65,7 +65,7 @@ resource "aws_subnet" "tf_private_subnet" {
   count                   = 2
   vpc_id                  = "${var.vpc_id}"
   cidr_block              = "${var.vpc_private_cidrs[count.index]}"
-  map_private_ip_on_launch = true
+#  map_private_ip_on_launch = true
   availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
 
   tags {
